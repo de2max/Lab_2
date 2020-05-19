f068a43475003a9633ec9539632073ab47b195be
Ira Mashtalir <mashtalir.i.2020@gmail.com>
2020-05-19 11:35:27 +0300

    BuisnesCard

---
+++ MyPurse/MyPurse/BuisnesCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPurse
+{
+    class BuisnesCard
+    {
+        protected string money;
+
+        public BuisnesCard(string num)
+        {
+            this.money = num;
+        }
+
+        public string info() {
+            return this.money;
+        }
+    }
+}
